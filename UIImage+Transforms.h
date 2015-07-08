79c8b7b5e4377b8bd08406565e7922811f70622a
Jameel <i.jameelkhan@gmail.com>
2015-07-09 02:10:30 +0500

    more methods added

--- UIImage+Transforms.h
+++ UIImage+Transforms.h
@@ -23,4 +23,21 @@
 - (UIImage *)colorizeImageWithColor:(UIColor *)color withBlendMode:(CGBlendMode)blendMode;
 + (UIImage*) maskImage:(UIImage *)image withMask:(UIImage *)maskImage ;
 
+
+
+
+
+- (UIImage *)croppedImage:(CGRect)bounds;
+- (UIImage *)thumbnailImage:(NSInteger)thumbnailSize
+          transparentBorder:(NSUInteger)borderSize
+               cornerRadius:(NSUInteger)cornerRadius
+       interpolationQuality:(CGInterpolationQuality)quality;
+- (UIImage *)resizedImage:(CGSize)newSize
+     interpolationQuality:(CGInterpolationQuality)quality;
+- (UIImage *)resizedImageWithContentMode:(UIViewContentMode)contentMode
+                                  bounds:(CGSize)bounds
+                    interpolationQuality:(CGInterpolationQuality)quality;
+
+
+
 @end
